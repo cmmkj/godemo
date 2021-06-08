7dbe1a3db2ed39a9f1e1f31dfc586d442ff39c83
yuxinhua <18927470456@163.com>
2021-06-08 11:55:22 +0800

    resstructure slice

--- sum/sum.go
+++ sum/sum.go
@@ -1,9 +1,9 @@
 package main
 
-func Sum(numbers [5]int) (sum int) {
-	sum = 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
