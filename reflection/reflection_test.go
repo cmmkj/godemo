760bc7c3a1b9752f2b0ab4ef57f3fcdac2b30217
yuxinhua <18927470456@163.com>
2021-06-17 19:34:40 +0800

    reflect int

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -32,9 +32,9 @@ func TestWalk(t *testing.T) {
 			"Struct with one string field",
 			struct {
 				Name string
-				City string
-			}{"Chris", "London"},
-			[]string{"Chris", "London"},
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
 		},
 	}
 	for _, test := range cases {
