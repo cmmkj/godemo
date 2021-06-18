30480d6dc54f0b65cbeb9d2ab4c101ab8374e463
yuxinhua <18927470456@163.com>
2021-06-18 15:29:24 +0800

    404 test

--- http_server/server.go
+++ http_server/server.go
@@ -6,15 +6,56 @@ import (
 	"net/http"
 )
 
-type HandlerFunc func(ResponseWriter, *Request)
+//type HandlerFunc func(ResponseWriter, *Request)
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+type InMemoryPlayerStore struct {
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+// func PlayerServer(w http.ResponseWriter, r *http.Request) {
+// 	player := r.URL.Path[len("/players/"):]
+// 	fmt.Fprint(w, GetPlayerScore(player))
+// }
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+	if name == "Floyd" {
+		return "10"
+	}
+	return ""
 }
 
 func main() {
-	handler := http.HandlerFunc(PlayerServer)
-	if err := http.ListenAndServe(":5000", handler); err != nil {
+	//handler := http.HandlerFunc(PlayerServer)
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
 }
