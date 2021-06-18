97d6c3709b6cecf43009891ab461a459a72107ec
yuxinhua <18927470456@163.com>
2021-06-18 11:57:55 +0800

    fix

---
+++ http_server/server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type HandlerFunc func(ResponseWriter, *Request)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "20")
+}
+
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
